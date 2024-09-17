6472d253d11a78641382e7537cafe028dd338e75
Shaid Hussain <shaid.hussain@nagarro.com>
2024-09-17 15:43:53 +0530

    done

--- payment-service/src/main/java/com/payment/saga/OrderCreatedEvent.java
+++ payment-service/src/main/java/com/payment/saga/OrderCreatedEvent.java
@@ -1,6 +1,5 @@
 package com.payment.saga;
 
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -8,7 +7,6 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
-//@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY, property = "@class")
 public class OrderCreatedEvent {
     private Long orderId;
     private double amount;
