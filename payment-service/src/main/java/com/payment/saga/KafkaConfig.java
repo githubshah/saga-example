78de90aaed2ca8f9c2a9b2382f547099a7e848d5
Shaid Hussain <shaid.hussain@nagarro.com>
2024-09-18 12:21:52 +0530

    kafka ui

--- payment-service/src/main/java/com/payment/saga/KafkaConfig.java
+++ payment-service/src/main/java/com/payment/saga/KafkaConfig.java
@@ -7,6 +7,7 @@ import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -29,6 +30,9 @@ import java.util.Map;
 @EnableKafka
 public class KafkaConfig {
 
+    @Value("${KAFKASERVER}")
+    private String KAFKA_SERVER;
+
     @Bean
     public NewTopic topic1() {
         return TopicBuilder.name("order-created")
@@ -46,14 +50,16 @@ public class KafkaConfig {
     }
 
     @Bean
-    public KafkaAdmin.NewTopics topics() {
-        return new KafkaAdmin.NewTopics(topic1(), topic2());
+    public KafkaAdmin kafkaAdmin() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_SERVER);
+        return new KafkaAdmin(configProps);
     }
 
     @Bean
     public ProducerFactory<String, Object> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_SERVER);
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         return new DefaultKafkaProducerFactory<>(configProps);
@@ -67,7 +73,7 @@ public class KafkaConfig {
     @Bean
     public ConsumerFactory<String, Object> consumerFactory() {
         Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_SERVER);
         configProps.put(ConsumerConfig.GROUP_ID_CONFIG, "group_id");
         configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
